ba9143be42cf1b1787dbdceb762f8dc0b1a6dfb5
ViniciusP-Santos <viniciuspereira2244@gmail.com>
2024-11-05 21:31:22 -0300

    feat: add authenticator with clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  return (<h1>Hello World</h1>
+const HomePage = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+
+  return (
+    <div className="flex h-full items-center justify-center">
+      <UserButton showName />
+    </div>
   );
-}
+};
+
+export default HomePage;
