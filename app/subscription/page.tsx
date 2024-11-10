d86d3a9022bd486b048caf99bf1751471a73a209
ViniciusP-Santos <viniciuspereira2244@gmail.com>
2024-11-10 20:20:47 -0300

    feat: ensure user can not create transaction if above limit

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -5,6 +5,7 @@ import { auth, clerkClient } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import AquirePlanButton from "./_components/aquire-plan-button";
 import { Badge } from "../_components/ui/badge";
+import getCurrentMonthTransactions from "../_data/get-currentMonthTransactions";
 
 const SubscriptionPage = async () => {
   const { userId } = await auth();
@@ -13,6 +14,7 @@ const SubscriptionPage = async () => {
   }
 
   const user = await clerkClient().users.getUser(userId);
+  const currentMonthTransactions = await getCurrentMonthTransactions();
   const hasPremiumPlan = user?.publicMetadata?.subscriptionPlan === "premium";
   return (
     <>
@@ -37,7 +39,9 @@ const SubscriptionPage = async () => {
               <CardContent className="space-y-6 py-8">
                 <div className="flex items-center gap-2">
                   <CheckIcon className="text-primary" />
-                  <p>Apenas 10 transações por mês (7/10)</p>
+                  <p>
+                    Apenas 10 transações por mês ({currentMonthTransactions}/10)
+                  </p>
                 </div>
                 <div className="flex items-center gap-2">
                   <XIcon />
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -5,6 +5,7 @@ import AddTransactionButton from "../_components/add-transaction-button";
 import Navbar from "../_components/navbar";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import canUserAddTransaction from "../_data/can-user-add-transaction";
 
 const TransactionsPage = async () => {
   const { userId } = await auth();
@@ -17,14 +18,14 @@ const TransactionsPage = async () => {
       userId,
     },
   });
-
+  const userCanAddTransaction = await canUserAddTransaction();
   return (
     <>
       <Navbar />
-      <div className="mt-20space-y-6 p-6">
+      <div className="mt-20 space-y-6 p-6">
         <div className="flex w-full items-center justify-between">
           <h1 className="text-2xl font-bold">Transações</h1>
-          <AddTransactionButton />
+          <AddTransactionButton userCanAddTransaction={userCanAddTransaction} />
         </div>
         <DataTable columns={transactionColumns} data={transactions} />
       </div>
